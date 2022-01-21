1303975a61b412c47dc087940c0e31704c9f7bd8
plasmaa0 <plasmaa0@gmail.com>
2022-01-21 15:56:28 +0300

    compiler optimisation flag added

--- run.bat
+++ run.bat
@@ -2,5 +2,5 @@ echo off
 del variables.txt
 del variables
 del build.exe
-g++ -Wall main.cpp expression.cpp symbol.cpp dictionary.cpp BET.cpp util.cpp output.cpp input.cpp calculator.cpp -o build.exe
+g++ -Wall -O0 main.cpp expression.cpp symbol.cpp dictionary.cpp BET.cpp util.cpp output.cpp input.cpp calculator.cpp -o build.exe
 build.exe
