b302739885d090eb0052b35c61d8056d2aeb98af
plasmaa0 <plasmaa0@gmail.com>
2022-02-08 21:05:31 +0300

    testing

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    cout << "program start" << endl;
     unsigned int dictionarySize = 30;
     CalculatorInit(dictionarySize);
     return 0;
